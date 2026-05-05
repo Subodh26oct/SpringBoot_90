77cc5bfcc8ca144ead21620c9432dde9304be866
Subodh26oct <subodh261003kumar@gmail.com>
2026-05-05 19:17:45 +0530

    first day

---
+++ src/main/java/com/subodh/sprintboot90days/springbootPlacement/SpringbootPlacementApplication.java
@@ -0,0 +1,13 @@
+package com.subodh.sprintboot90days.springbootPlacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPlacementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subodh/sprintboot90days/springbootPlacement/SpringbootPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subodh.sprintboot90days.springbootPlacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
