184711638f8d21aef7c87cc9ef2ac6e70d4986de
Subodh26oct <subodh261003kumar@gmail.com>
2026-05-06 23:05:16 +0530

    1.2

---
+++ src/main/java/com/subodh/sprintboot90days/springbootPlacement/Apple.java
@@ -0,0 +1,9 @@
+package com.subodh.sprintboot90days.springbootPlacement;
+
+
+public class Apple {
+
+    void eatApple(){
+        System.out.println("I am eating the apple ");
+    }
+}
---
+++ src/main/java/com/subodh/sprintboot90days/springbootPlacement/SpringbokPlacementApplication.java
@@ -0,0 +1,24 @@
+package com.subodh.sprintboot90days.springbootPlacement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbokPlacementApplication implements CommandLineRunner {
+
+	@Autowired
+	Apple obj;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbokPlacementApplication.class, args);
+
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		obj.eatApple();
+	}
+}
