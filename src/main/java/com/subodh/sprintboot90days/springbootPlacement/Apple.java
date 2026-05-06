2175ed858b68b4e1d4e0fbcfff6b4fde39e7dc25
Subodh26oct <subodh261003kumar@gmail.com>
2026-05-06 23:50:53 +0530

    1.3

--- src/main/java/com/subodh/sprintboot90days/springbootPlacement/Apple.java
+++ src/main/java/com/subodh/sprintboot90days/springbootPlacement/Apple.java
@@ -1,9 +1,16 @@
 package com.subodh.sprintboot90days.springbootPlacement;
 
 
+import jakarta.annotation.PostConstruct;
+
 public class Apple {
 
     void eatApple(){
         System.out.println("I am eating the apple ");
+    }
+
+    @PostConstruct
+    void callThisBeforeAppleIsUsed(){
+        System.out.println("Creating the apple before use");
     }
 }
