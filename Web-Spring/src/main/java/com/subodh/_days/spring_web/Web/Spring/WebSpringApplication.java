9b53546c4f12a69364c0b7bfbf8eda16b58acd43
Subodh26oct <subodh261003kumar@gmail.com>
2026-05-12 11:55:57 +0530

    Week 2

---
+++ Web-Spring/src/main/java/com/subodh/_days/spring_web/Web/Spring/WebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.subodh._days.spring_web.Web.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringApplication.class, args);
+	}
+
+}
